b5eff0538234940d5a7e4ecae869f8bffc21a3e6
kagerka <kagerka@gmail.com>
2023-12-08 21:05:20 +0300

    feat: add burger menu

---
+++ coffee-house/script.js
@@ -0,0 +1,22 @@
+let burger = document.querySelector('.header__burger');
+let burgerMenu = document.querySelector('.burger__menu');
+let headerNavLinks = document.querySelector('.header__nav');
+let headerNavMenuLink = document.querySelector('.header__nav.menu-link');
+let body = document.body;
+let link = document.querySelectorAll('.header__link');
+
+burger?.addEventListener('click', e => {
+  link.forEach(a => a.addEventListener('click', e => {
+    burger?.classList.remove('active');
+    burgerMenu?.classList.remove('active');
+    headerNavLinks?.classList.remove('active');
+    headerNavMenuLink?.classList.remove('active');
+    body.classList.remove('active');
+  }))
+  burger?.classList.toggle('active');
+  burgerMenu?.classList.toggle('active');
+  headerNavLinks?.classList.toggle('active');
+  headerNavMenuLink?.classList.toggle('active');
+  body.classList.toggle('active');
+})
+
