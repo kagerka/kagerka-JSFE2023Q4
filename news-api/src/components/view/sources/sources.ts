7b68810885088982098a16dc378999e090724b12
kagerka <kagerka@gmail.com>
2024-02-26 12:24:02 +0300

    refactor: add types to news.ts and sources.ts files

---
+++ news-api/src/components/view/sources/sources.ts
@@ -0,0 +1,34 @@
+import { assertIsDefined } from '../../../types/checking';
+import { SourceItem } from '../../../types/types';
+import './sources.css';
+
+class Sources {
+  draw(data: SourceItem[]) {
+    const fragment: DocumentFragment = document.createDocumentFragment();
+    const sourceItemTemp: HTMLTemplateElement | null = document.querySelector('#sourceItemTemp');
+    assertIsDefined(sourceItemTemp);
+
+    data.forEach((item: SourceItem) => {
+      const sourceClone: Node = sourceItemTemp.content.cloneNode(true);
+      if (!(sourceClone instanceof DocumentFragment)) {
+        throw new Error();
+      }
+
+      const sourceItemName = sourceClone.querySelector('.source__item-name');
+      assertIsDefined(sourceItemName);
+      sourceItemName.textContent = item.name;
+
+      const sourceItem = sourceClone.querySelector('.source__item');
+      assertIsDefined(sourceItem);
+      sourceItem.setAttribute('data-source-id', item.id);
+
+      fragment.append(sourceClone);
+    });
+
+    const sources = document.querySelector('.sources');
+    assertIsDefined(sources);
+    sources.append(fragment);
+  }
+}
+
+export default Sources;
---
+++ news-api/src/types/types.ts
@@ -0,0 +1,36 @@
+export interface SourceAPI {
+  status: 'ok' | 'error';
+  sources: SourceItem[];
+}
+
+export interface SourceItem {
+  id: string;
+  name: string;
+  description: string;
+  url: string;
+  category: string;
+  language: string;
+  country: string;
+}
+
+export interface NewsAPI {
+  status: 'ok' | 'error';
+  totalResults: number;
+  articles: Article[];
+}
+
+export interface Article {
+  source: ArticleSource;
+  author: string | null;
+  title: string | null;
+  description: string | null;
+  url: string;
+  urlToImage: string | null;
+  publishedAt: string;
+  content: string;
+}
+
+export interface ArticleSource {
+  id: string | null;
+  name: string;
+}
