3a9b90aa0905ed0486fe707481f44feaa97f8d65
Ekaterina Kotliarenko <kagerka@gmail.com>
2024-03-14 23:56:50 +0300

    feat: RSS-PZ-05 design start screen (#36)

--- rss-puzzle/src/pages/start-page/start-page.ts
+++ rss-puzzle/src/pages/start-page/start-page.ts
@@ -1,7 +1,36 @@
+import './start-page.scss';
+import icon from '../../assets/logo.svg';
+import { Button } from '../../module/button/button';
+
+const startButtonData = {
+  buttonName: 'Start',
+  type: 'submit',
+  className: 'start-btn',
+  id: 'startButton',
+};
+
 export const StartPage = (pageWrapper: HTMLElement) => {
   pageWrapper.textContent = '';
+
+  const contentWrapper = document.createElement('div');
+  contentWrapper.classList.add('content-wrapper');
+
   const heading = document.createElement('h1');
-  heading.textContent = 'Welcome';
+  heading.textContent = 'English puzzle';
   heading.classList.add('heading');
-  pageWrapper.append(heading);
+
+  const descriptionIcon = new Image();
+  descriptionIcon.src = icon;
+  descriptionIcon.alt = 'description icon';
+  descriptionIcon.classList.add('description-icon');
+
+  const description = document.createElement('p');
+  description.innerText =
+    'Learn English language in our puzzle game. Click on words, collect phrases and you will see beautiful pictures of famous artists. Words in the task can be drag and drop. You can use hints through the game, if the task too difficult for you now.';
+  description.classList.add('description');
+  pageWrapper.append(contentWrapper);
+
+  const startButton = Button(startButtonData);
+
+  contentWrapper.append(heading, descriptionIcon, description, startButton);
 };
