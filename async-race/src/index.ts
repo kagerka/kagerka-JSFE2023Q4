ac7418c544009c8f760631eda8d71c221ef1fedc
kagerka <kagerka@gmail.com>
2024-03-26 10:55:54 +0300

    feat: implement main page

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -1,4 +1,8 @@
 import { Header } from './components/header/header';
+import { Logo } from './components/logo/logo';
+import { MainContent } from './components/main-content/main-content';
 import './styles/styles.scss';
 
+new Logo().render(document.body);
 new Header().render(document.body);
+new MainContent().render(document.body);
