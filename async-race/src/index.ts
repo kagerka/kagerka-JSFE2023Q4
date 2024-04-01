8274453363003cdde599c4f65c95b2325309f6dc
kagerka <kagerka@gmail.com>
2024-04-01 20:25:10 +0300

    refactor: code refactor

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -4,6 +4,5 @@ import { MainContent } from './components/main-content/main-content';
 import './styles/styles.scss';
 
 const logo = new Logo().render(document.body);
-
 new Header().render(document.body);
 new MainContent(logo).render(document.body);
