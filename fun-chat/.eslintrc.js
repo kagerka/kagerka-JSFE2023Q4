6ad712f2705720650ae4aece6356dbd5874b0d46
kagerka <kagerka@gmail.com>
2024-04-12 12:11:25 +0300

    chore: configure webpack and plugins

---
+++ fun-chat/.eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: [
+    "eslint-config-airbnb-base",
+    "airbnb-typescript/base",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "prettier",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  noInlineConfig: true,
+  plugins: ["@typescript-eslint", "import", "prettier"],
+  rules: {
+    "no-console": "warn",
+    "import/prefer-default-export": "off",
+    "no-param-reassign": [
+      "error",
+      {
+        props: false,
+      },
+    ],
+    "no-magic-numbers": ["error", { ignoreArrayIndexes: true }],
+    "@typescript-eslint/explicit-function-return-type": "error",
+    "max-lines-per-function": ["error", { max: 40, skipBlankLines: true }],
+    "max-len": "off",
+  },
+};
---
+++ fun-chat/webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: './src/index.ts',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|svg|webp)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/img/[name][ext]',
+        },
+      },
+      {
+        test: /\.(c|sa|sc)ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      img: path.join(__dirname, 'src', 'assets', 'img'),
+    },
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    assetModuleFilename: 'assets/[name][ext]',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+      favicon: path.resolve(__dirname, 'src', 'assets', 'favicon.png'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/assets/img'),
+          to: path.resolve(__dirname, 'dist/assets/img'),
+        },
+      ],
+    }),
+    new ESLintPlugin({
+      extensions: ['ts'],
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 4000,
+  },
+};
