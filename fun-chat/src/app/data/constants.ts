81c6dcdb27da62ef07feeda98b7673c6e9833616
kagerka <kagerka@gmail.com>
2024-04-22 21:28:08 +0300

    feat: add constants and types

--- fun-chat/src/app/data/constants.ts
+++ fun-chat/src/app/data/constants.ts
@@ -1,2 +1,5 @@
 export const MIN_INPUT_LENGTH = 4;
 export const HISTORY_BACK = -1;
+
+export const ZERO = 0;
+export const ONE = 1;
--- fun-chat/src/app/data/types.ts
+++ fun-chat/src/app/data/types.ts
@@ -8,7 +8,53 @@ export type ButtonType = {
   styles: string[];
 };
 
-export type RouterType = {
-  path: string;
-  component: string;
+export type AuthType = {
+  id: string;
+  type: string;
+  payload: CurrentUserType;
+};
+
+export type CurrentUserType = {
+  user: CurrentUserInfoType;
+};
+
+export type CurrentUserInfoType = {
+  login: string;
+  password: string;
+};
+
+export type AllUsersType = {
+  id: string;
+  type: string;
+  payload: UserType;
+};
+
+export type UserType = {
+  user: UserInfoType;
+};
+
+export type UserInfoType = {
+  login: string;
+  isLogined: boolean;
+};
+
+export type MessageResponseType = {
+  id: string;
+  type: string;
+  payload: MessagePayloadType;
+};
+
+export type MessagePayloadType = {
+  message: {
+    id: string;
+    from: string;
+    to: string;
+    text: string;
+    datetime: number;
+    status: {
+      isDelivered: boolean;
+      isReaded: boolean;
+      isEdited: boolean;
+    };
+  };
 };
